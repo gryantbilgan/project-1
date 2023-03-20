8eb6fc45191b20a74de4e71497d2e1f32de4324f
gryantgilgan <bryantgilgan@gmail.com>
2023-03-19 23:17:45 -0700

    solutions and errors

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,7 @@ function setGame() {
         let number = document.createElement("div");
         number.id = i
         number.innerText = i;
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
@@ -60,12 +61,66 @@ function setGame() {
     // Use concatenation to string row and column
     // Add  tile class and style
     // Append tile to gameboard
+    // Add event at tile
+    // Display row and column index on board
+    // Remove dashes with condition
+    // Add style for tile-start
+    // Add horizontal and vertical line and style
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString
+            if (gameboard[r][c] != "-") {
+                tile.innerText = gameboard[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("gameboard").append(tile);
+        }
+    }
+}
+
+// Add funcitonality to game
+// Allow user to select numbers
+// Tie function to div elements
+// Add number-selected class and style
+// Toggle highlight of number-selcted on and off
+function selectNumber() {
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+// Add function for select tile and add number
+// When clicking on tile make sure number selected first
+// Add condiditonal to keep from overriding perviously placed number
+// Split string coordinates into new array without dash
+// Compare number input with solution board
+// Parse the value of the row and column and return a number for coordinates
+// If number input doesn't match solution then errors display and won't inout number
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+        let coords = this.id.split("-");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if(solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        } else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
         }
     }
 }
