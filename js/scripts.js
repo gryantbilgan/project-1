d59a645e6e210a2f4f7b06bb2ba97a42bc30efdb
gryantgilgan <bryantgilgan@gmail.com>
2023-03-19 19:45:35 -0700

    gameboard index

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// Build arrays and strings for numbers placed on the gameboard as well as the solution
+// Establish indexes for each tile
+var numSelected = null;
+var tileSelected = null;
+
+// Error counter
+var errors = 0;
+
+// Gameboard Index
+var gameboard = [
+    "--2-8--6-",
+    "-56917-3-",
+    "-4--5-871",
+    "-9----6--",
+    "671-952--",
+    "----2-1--",
+    "167-3-59-",
+    "48--7-3--",
+    "-2546----"
+]
+
+// Solution Index
+var solution [
+    
+]
