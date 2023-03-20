b4149b9601abd682306f8c12e5c9cdfc7c21ccc5
gryantgilgan <bryantgilgan@gmail.com>
2023-03-19 21:13:44 -0700

    gimme them digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,49 @@
 // Build arrays and strings for numbers placed on the gameboard as well as the solution
 // Establish indexes for each tile
-var numSelected = null;
-var tileSelected = null;
+// var numSelected = null;
+// var tileSelected = null;
 
-// Error counter
-var errors = 0;
+// // Error counter
+// var errors = 0;
 
-// Gameboard Index
-var gameboard = [
-    "--2-8--6-",
-    "-56917-3-",
-    "-4--5-871",
-    "-9----6--",
-    "671-952--",
-    "----2-1--",
-    "167-3-59-",
-    "48--7-3--",
-    "-2546----"
-]
+// // Gameboard Index
+// var gameboard = [
+//     "--2-8--6-",
+//     "-56917-3-",
+//     "-4--5-871",
+//     "-9----6--",
+//     "671-952--",
+//     "----2-1--",
+//     "167-3-59-",
+//     "48--7-3--",
+//     "-2546----"
+// ]
 
-// Solution Index
-var solution [
-    
-]
+// // Solution Index
+// var solution = [
+//     "712384965",
+//     "856917432",
+//     "943652871",
+//     "298143657",
+//     "671895243",
+//     "534726189",
+//     "167238594",
+//     "489571326",
+//     "325469718"
+// ]
+
+// // Load the gameboard as soon as the page has loaded
+// window.onload = function() {
+//     setGame();
+// }
+
+// // Create a div tag <div id="1">1</div>
+// // Use for loop for "digits 1-9"
+// // Display the numbers
+// function setGame() {
+//     for (let i = 0; i < 9; i++) {
+//         let number = document.createElement("div");
+//         number.id = i
+//     }
+// }
+console.log("gokushufudo!")
